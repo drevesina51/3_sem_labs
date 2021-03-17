78e47fb860baa03546204a95a451886067fb28dd
IRONy <71219611+drevesina51@users.noreply.github.com>
2021-03-17 21:01:28 +0300

    Create queue.h

---
+++ lab2/queue.h
@@ -0,0 +1,16 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+#include "list.h"
+
+typedef struct Queue {
+    List *list;
+} Queue;
+
+Queue *queue_new(Queue *queue);
+void queue_push_front(Queue *queue, char *id, int ta, int ts);
+void queue_push_back(Queue *queue, char *id, int ta, int ts);
+int queue_remove(Queue *queue, char *id);
+void queue_print_time(Queue *queue, int time);
+Data *queue_find(Queue *queue, int time);
+
+#endif //QUEUE_H
