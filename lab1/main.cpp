d3fb80db3bb9742e8e20ec71a804bf16ca7a3178
IRONy <71219611+drevesina51@users.noreply.github.com>
2021-09-19 19:19:55 +0300

    Update main.cpp

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -9,14 +9,26 @@ using namespace Lab1;
 
 int main() {
     Matrix *matrix = input();
+    int i = 1;
     if (!matrix) {
         std::cout << "incorrect data" << std::endl;
         return 1;
     }
-    output("Sourced matrix", matrix);
-    task(matrix);
-    output("Result matrix", matrix);
+    std::cout << "\nselect matrix format:\n1) with 0\n2) without 0\n";
+    if (getNum(i) < 0) {
+        return 1;
+    }
+    if (i == 1) output_with("\nSourced matrix", matrix);
+    else output_without("\nSourced matrix", matrix);
+    Matrix* res = copy(matrix);
+    if (!res) {
+        return 1;
+    }
+    task(res);
+    if (i == 1) output_with("\nResult matrix", res);
+    else output_without("\nResult matrix", res);
     erase(matrix, matrix->m); // освобождение памяти
+    erase(res, res->m);
     _CrtDumpMemoryLeaks();
     return 0;
 }
